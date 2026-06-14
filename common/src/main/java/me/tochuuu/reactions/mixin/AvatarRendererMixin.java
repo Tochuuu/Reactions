0b925e8a1b21a85033c1820d5638268eb26ef12f
Tochuuu <antekhoff@wp.pl>
2026-06-14 14:01:08 +0200

    Add delayed action face animations

--- common/src/main/java/me/tochuuu/reactions/mixin/AvatarRendererMixin.java
+++ common/src/main/java/me/tochuuu/reactions/mixin/AvatarRendererMixin.java
@@ -1,11 +1,13 @@
 package me.tochuuu.reactions.mixin;
 
 import me.tochuuu.reactions.client.PlayerEyeRenderLayer;
+import me.tochuuu.reactions.client.PlayerActionAnimationState;
 import net.minecraft.client.model.player.PlayerModel;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.player.AvatarRenderer;
 import net.minecraft.client.renderer.entity.state.AvatarRenderState;
+import net.minecraft.world.entity.Avatar;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,5 +20,10 @@ public abstract class AvatarRendererMixin {
         RenderLayerParent<AvatarRenderState, PlayerModel> parent = (RenderLayerParent<AvatarRenderState, PlayerModel>) (Object) this;
         LivingEntityRendererAccessor<AvatarRenderState, PlayerModel> accessor = (LivingEntityRendererAccessor<AvatarRenderState, PlayerModel>) (Object) this;
         accessor.reactions$addLayer(new PlayerEyeRenderLayer(parent));
+    }
+
+    @Inject(method = "extractRenderState(Lnet/minecraft/world/entity/Avatar;Lnet/minecraft/client/renderer/entity/state/AvatarRenderState;F)V", at = @At("TAIL"))
+    private void reactions$captureActionState(Avatar player, AvatarRenderState state, float partialTick, CallbackInfo ci) {
+        PlayerActionAnimationState.capture(player, state);
     }
 }
